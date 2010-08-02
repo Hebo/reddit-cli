1d1487337e7d5a496ab64203efc80078ca3953dd
Cev <doubletime@gmail.com>
2010-08-01 23:35:41 -0400

    Encapsulated listing widget and code cleanup

--- pages.py
+++ pages.py
@@ -8,14 +8,12 @@ def smart_truncate(content, length=100, suffix='...'):
 	else:
 		return ' '.join(content[:length+1].split(' ')[0:-1]) + suffix
 
+
 class Story:
 	"""listing of a single reddit story"""
 	def __init__(self, object):
 		"""create story from dict object representation"""
 		self.object = object
-		
-		# replace "smart quotes" which cause encode to choke
-		self.title = self.title.replace(u"\u2019", "'")
 		
 
 	def __getattr__(self, name):
@@ -25,34 +23,36 @@ class Story:
 		else:
 			raise AttributeError, name
 
-	def __str__(self):
-		"""prepare story as a two-line string"""
-		return "{0}\n{1} points   {2} comments   {3}   {4}".format(
-									smart_truncate(self.title.encode('utf-8'), length=76), 
+
+	def format_lines(self):
+		"""prepare story as a two-string tuple"""
+		line1 = "{0}".format(smart_truncate(self.title.encode('utf-8'), length=76))
+		line2 = "{0} points	  {1} comments	 {2}   {3}".format(
 									self.score,
 									self.num_comments,
 									self.domain,
 									"/r/" + self.subreddit,
 									)
+		return (line1, line2)
 
 		
-
 def get_stories(subreddit):
 	"""download json from reddit and return list of stories"""
+	if subreddit is None: 
+		url = "http://www.reddit.com/.json"
+	else: 
+		url = "http://www.reddit.com/r/" + subreddit + "/.json"
+	req = urllib2.Request( url )
+	opener = urllib2.build_opener()
 	try:
-		if subreddit is None: url = "http://www.reddit.com/.json"
-		else: url = "http://www.reddit.com/r/" + subreddit + "/.json"
-			
-		req = urllib2.Request( url )
-		opener = urllib2.build_opener()
 		f = opener.open(req)
 		stories_raw = json.loads(f.read())
-	
+
 		stories = []
 		for i in stories_raw['data']['children']:
 			stories.append(Story(i['data']))
 		return stories
 	except Exception as e:
-		raise
+		raise Exception, "Error getting reddit listings"
 	
 	
--- reddit.py
+++ reddit.py
@@ -1,33 +1,52 @@
 import urwid
 from pages import Story, get_stories
 
+class Listing(urwid.FlowWidget):
+	"""contains a single story and manages its events"""
+	def __init__(self, story):
+		self.story = story
+		self.lines = story.format_lines()
+	def selectable(self):
+		return True
+	def rows(self, size, focus=False):
+		return 2
+	def render(self, size, focus=False):
+		(maxcol,) = size
+		if focus:
+			pass
+		# pad lines to column width
+		fill = lambda x: x.ljust(maxcol)
+		return urwid.TextCanvas( text=list( map(fill, self.lines) ) )
+	def keypress(self, size, key):
+		return key
+
 class MainWindow(object):
 	"""manages main window elements"""
 	def __init__(self):
-		self.stories = []
+		self.listings = []
 		self.load_stories()
 		
 	def load_stories(self, subreddit=None):
 		"""load or update stories from specified subreddit"""
-		self.stories = []
+		self.listings = []
 		for s in get_stories(subreddit):
-			current = urwid.Text( ('body', str(s)), wrap='clip' )
-			self.stories.append( urwid.Padding(current, left=1, right=1) )
+			current = Listing( s )
+			self.listings.append( urwid.Padding(current, left=1, right=1) )
 		
 	def get_widget(self):
-		"""return widget comprised of all stories"""
-		stories_formatted = self.stories[:]
+		"""return widget comprised of all listings"""
+		listings_formatted = self.listings[:]
 		# Separate stories with blank line
-		for story in stories_formatted[:]:
-			index = stories_formatted.index(story)
-			if index != len(stories_formatted) - 1:
-				stories_formatted.insert(index + 1, urwid.Divider(" "))
-		stories_formatted.insert(0, urwid.Divider(" "))
+		for story in listings_formatted[:]:
+			index = listings_formatted.index(story)
+			if index != len(listings_formatted) - 1:
+				listings_formatted.insert(index + 1, urwid.Divider(" "))
+		listings_formatted.insert(0, urwid.Divider(" "))
 		# Highlight stories when focused
-		self.stories_active = urwid.ListBox( urwid.SimpleListWalker(
-							[urwid.AttrMap(w, None, 'focus') for w in stories_formatted]
+		self.listings_active = urwid.ListBox( urwid.SimpleListWalker(
+							[urwid.AttrMap(w, None, 'focus') for w in listings_formatted]
 							))
-		return self.stories_active
+		return self.listings_active
 
 
 def main():
@@ -39,7 +58,7 @@ def main():
 				]
 
 	# Set up header and footer ui widgets 
-	header_content = urwid.Text(('header', "RedditCLI - http://github.com/cev/redditcli"), align='center')
+	header_content = urwid.Text(('header', "reddit-cli - http://github.com/cev/reddit-cli"), align='center')
 	footer_content = urwid.Text(('footer', "status: reddit gold required")) 
 	footer_content = urwid.Padding(footer_content, left=1, right=1)
 		
@@ -61,7 +80,7 @@ def main():
 					raise Exception, (focus.index(), dir(focus.index))
 			if key == 'enter':
 				raise urwid.ExitMainLoop()
-			#return keys
+			return keys
 
 	# Start ui 
 	loop = urwid.MainLoop(frame, palette, input_filter=input_handler)
