7012dcb22e8a9470f19a2b6e8e3c8dfdceb3ca7d
Cev <doubletime@gmail.com>
2010-08-02 17:22:53 -0400

    Tabs to spaces!

--- pages.py
+++ pages.py
@@ -3,80 +3,80 @@ import json
 import re, htmlentitydefs #html escaping
 
 def smart_truncate(content, length=100, suffix='...'):
-	"""truncate on word boundaries"""
-	if len(content) <= length:
-		return content
-	else:
-		return ' '.join(content[:length+1].split(' ')[0:-1]) + suffix
+    """truncate on word boundaries"""
+    if len(content) <= length:
+        return content
+    else:
+        return ' '.join(content[:length+1].split(' ')[0:-1]) + suffix
 
 
 def unescape(text):
-	"""Remove HTML or XML character references and entities from a text string"""
-	def fixup(m):
-		text = m.group(0)
-		if text[:2] == "&#":
-			# character reference
-			try:
-				if text[:3] == "&#x":
-					return unichr(int(text[3:-1], 16))
-				else:
-					return unichr(int(text[2:-1]))
-			except ValueError:
-				pass
-		else:
-			# named entity
-			try:
-				text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
-			except KeyError:
-				pass
-		return text # leave as is
-	return re.sub("&#?\w+;", fixup, text)
-		
-		
+    """Remove HTML or XML character references and entities from a text string"""
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text # leave as is
+    return re.sub("&#?\w+;", fixup, text)
+        
+        
 class Story:
-	"""holds json data of a single reddit story"""
-	def __init__(self, object):
-		"""create story from dict object representation"""
-		assert isinstance(object, dict), "json object is not a dict: %s" % type(object)
-		self.object = object
-		
-	def __getattr__(self, name):
-		"""pull elements directly from the stored object"""
-		if self.object.has_key(name):
-			return self.object.get(name)
-		else:
-			raise AttributeError, name
+    """holds json data of a single reddit story"""
+    def __init__(self, object):
+        """create story from dict object representation"""
+        assert isinstance(object, dict), "json object is not a dict: %s" % type(object)
+        self.object = object
+        
+    def __getattr__(self, name):
+        """pull elements directly from the stored object"""
+        if self.object.has_key(name):
+            return self.object.get(name)
+        else:
+            raise AttributeError, name
 
-	def format_lines(self):
-		"""prepare story as a two-string tuple"""
-		line1 = "{0}".format(unescape(
-							smart_truncate(self.title.encode('utf-8'), length=76)
-							))
-		line2 = "{0} points   {1} comments   {2}   {3}".format(
-									self.score,
-									self.num_comments,
-									self.domain,
-									"/r/" + self.subreddit,
-									)
-		return (line1, line2)
+    def format_lines(self):
+        """prepare story as a two-string tuple"""
+        line1 = "{0}".format(unescape(
+                            smart_truncate(self.title.encode('utf-8'), length=76)
+                            ))
+        line2 = "{0} points   {1} comments   {2}   {3}".format(
+                                    self.score,
+                                    self.num_comments,
+                                    self.domain,
+                                    "/r/" + self.subreddit,
+                                    )
+        return (line1, line2)
 
-		
+        
 def get_stories(subreddit):
-	"""download json from reddit and return list of stories"""
-	if subreddit is None: 
-		url = "http://www.reddit.com/.json"
-	else: 
-		url = "http://www.reddit.com/r/" + subreddit + "/.json"
-	req = urllib2.Request( url )
-	opener = urllib2.build_opener()
-	try:
-		f = opener.open(req)
-		stories_raw = json.loads(f.read())
-	except urllib2.HTTPError as e:
-		raise e, "HTTPError getting reddit listings"
-	stories = []
-	for i in stories_raw['data']['children']:
-		stories.append(Story(i['data']))
-	return stories
-	
-	
+    """download json from reddit and return list of stories"""
+    if subreddit is None: 
+        url = "http://www.reddit.com/.json"
+    else: 
+        url = "http://www.reddit.com/r/" + subreddit + "/.json"
+    req = urllib2.Request( url )
+    opener = urllib2.build_opener()
+    try:
+        f = opener.open(req)
+        stories_raw = json.loads(f.read())
+    except urllib2.HTTPError as e:
+        raise e, "HTTPError getting reddit listings"
+    stories = []
+    for i in stories_raw['data']['children']:
+        stories.append(Story(i['data']))
+    return stories
+    
+    
--- reddit.py
+++ reddit.py
@@ -2,87 +2,87 @@ import urwid
 from pages import Story, get_stories
 
 class Listing(urwid.FlowWidget):
-	"""contains a single story and manages its events"""
-	def __init__(self, story):
-		self.story = story
-		self.lines = story.format_lines()
-	def selectable(self):
-		return True
-	def rows(self, size, focus=False):
-		return 2
-	def render(self, size, focus=False):
-		(maxcol,) = size
-		if focus:
-			pass
-		# pad lines to column width
-		fill = lambda x: x.ljust(maxcol)
-		return urwid.TextCanvas(text=list(map(fill, self.lines)))
-	def keypress(self, size, key):
-		return key
+    """contains a single story and manages its events"""
+    def __init__(self, story):
+        self.story = story
+        self.lines = story.format_lines()
+    def selectable(self):
+        return True
+    def rows(self, size, focus=False):
+        return 2
+    def render(self, size, focus=False):
+        (maxcol,) = size
+        if focus:
+            pass
+        # pad lines to column width
+        fill = lambda x: x.ljust(maxcol)
+        return urwid.TextCanvas(text=list(map(fill, self.lines)))
+    def keypress(self, size, key):
+        return key
 
 class MainWindow(object):
-	"""manages main window elements"""
-	def __init__(self):
-		self.listings = []
-		self.load_stories()
-		
-	def load_stories(self, subreddit=None):
-		"""load or update stories from specified subreddit"""
-		self.listings = []
-		for s in get_stories(subreddit):
-			current = Listing(s)
-			self.listings.append(urwid.Padding(current, left=1, right=1))
-		
-	def get_widget(self):
-		"""return widget comprised of all listings"""
-		listings_formatted = self.listings[:]
-			
-		# Separate stories with blank line & highlight on focus
-		for (i, l) in enumerate(listings_formatted):
-			filled = urwid.Filler(urwid.AttrMap(l, None, 'focus'))
-			listings_formatted[i] = urwid.BoxAdapter(filled, 3)
-		listings_formatted.append(urwid.Divider("*"))
-		
-		self.listings_active = urwid.ListBox(urwid.SimpleListWalker(listings_formatted))
-		return self.listings_active
+    """manages main window elements"""
+    def __init__(self):
+        self.listings = []
+        self.load_stories()
+        
+    def load_stories(self, subreddit=None):
+        """load or update stories from specified subreddit"""
+        self.listings = []
+        for s in get_stories(subreddit):
+            current = Listing(s)
+            self.listings.append(urwid.Padding(current, left=1, right=1))
+        
+    def get_widget(self):
+        """return widget comprised of all listings"""
+        listings_formatted = self.listings[:]
+            
+        # Separate stories with blank line & highlight on focus
+        for (i, l) in enumerate(listings_formatted):
+            filled = urwid.Filler(urwid.AttrMap(l, None, 'focus'))
+            listings_formatted[i] = urwid.BoxAdapter(filled, 3)
+        listings_formatted.append(urwid.Divider("*"))
+        
+        self.listings_active = urwid.ListBox(urwid.SimpleListWalker(listings_formatted))
+        return self.listings_active
 
 
 def main():
-	palette =	[
-				('header', 'dark magenta,bold', 'default'),
-				('footer', 'white,bold', 'dark red'),
-				('body', 'light gray', 'default'),
-				('focus', 'black', 'dark cyan', 'standout')
-				]
+    palette =   [
+                ('header', 'dark magenta,bold', 'default'),
+                ('footer', 'white,bold', 'dark red'),
+                ('body', 'light gray', 'default'),
+                ('focus', 'black', 'dark cyan', 'standout')
+                ]
 
-	# Set up header and footer ui widgets 
-	header_content = urwid.Text(('header', "reddit-cli - http://github.com/cev/reddit-cli"), align='center')
-	footer_content = urwid.Text(('footer', "status: reddit gold required")) 
-	footer_content = urwid.Padding(footer_content, left=1, right=1)
-		
-	body = MainWindow()
-	body.load_stories()
-	
-	# Create frame for main window layout
-	main_widget = body.get_widget()
-	frame = urwid.Frame(	main_widget,
-							header=header_content, 
-							footer=footer_content )
+    # Set up header and footer ui widgets 
+    header_content = urwid.Text(('header', "reddit-cli - http://github.com/cev/reddit-cli"), align='center')
+    footer_content = urwid.Text(('footer', "status: reddit gold required")) 
+    footer_content = urwid.Padding(footer_content, left=1, right=1)
+        
+    body = MainWindow()
+    body.load_stories()
+    
+    # Create frame for main window layout
+    main_widget = body.get_widget()
+    frame = urwid.Frame(    main_widget,
+                            header=header_content, 
+                            footer=footer_content )
 
-	def input_handler(keys, raw):
-		for key in keys:
-			if key in ('j','k'):
-				focus = main_widget.get_focus()
-				print focus
-				if key == 'j':
-					raise Exception, (focus.index(), dir(focus.index))
-			if key == 'enter':
-				raise urwid.ExitMainLoop()
-			return keys
+    def input_handler(keys, raw):
+        for key in keys:
+            if key in ('j','k'):
+                focus = main_widget.get_focus()
+                print focus
+                if key == 'j':
+                    raise Exception, (focus.index(), dir(focus.index))
+            if key == 'enter':
+                raise urwid.ExitMainLoop()
+            return keys
 
-	# Start ui 
-	loop = urwid.MainLoop(frame, palette, input_filter=input_handler)
-	loop.run()
+    # Start ui 
+    loop = urwid.MainLoop(frame, palette, input_filter=input_handler)
+    loop.run()
 
 if __name__ == "__main__":
-	main()
+    main()
